caa855cc17bda5642c8f3c701fbbae07b72aa9ae
Chanseo Park <qhrn12345@naver.com>
2023-11-11 19:11:25 +0900

    final

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -6,22 +6,16 @@
 /*   By: chanspar <chanspar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 18:09:31 by chanspar          #+#    #+#             */
-/*   Updated: 2023/11/08 20:48:57 by chanspar         ###   ########.fr       */
+/*   Updated: 2023/11/10 16:58:31 by chanspar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	a(void)
-{
-	system("leaks fdf");
-}
-
 int	main(int ac, char *av[])
 {
 	t_info	info;
 
-	atexit(a);
 	if (ac != 2)
 		err_print("Usage : ./fdf <filename>");
 	check_right_file(&info, av);
