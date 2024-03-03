0c02e2a0294411611febaed8ffac4b85f3b08b50
chanspar <qhrn12345@naver.com>
2024-03-03 17:59:33 +0900

    module01 ~ex06

---
+++ cppModule/cpp01/ex04/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+bool argErr(int ac, char *av[])
+{
+	if (ac != 4) {
+		std::cout << "argument count isn't 4\n";
+		return false;
+	}
+	else if (!av[1][0]) {
+		std::cout << "No such file\n";
+		return false;
+	}
+	else if (!av[2][0]) {
+		std::cout << "empty string, err replace\n";
+		return false;
+	}
+	return true;
+}
+
+int main(int ac, char *av[])
+{
+	std::string filename;
+	std::string s1;
+	std::string s2;
+	std::ifstream inputFile;
+	std::ofstream outputFile;
+	std::string	replaceFile;
+	std::string line;
+	int s1Len;
+	int s2Len;
+
+	if (!argErr(ac, av)) {
+		return 1;
+	}
+
+	filename = av[1];
+	s1 = av[2];
+	s2 = av[3];
+	s1Len = s1.length();
+	s2Len = s2.length();
+
+	inputFile.open(filename);
+	if (!inputFile.is_open()) {
+		std::cout << "Failed to open the file.\n";
+		return (1); 
+	}
+	
+	replaceFile = av[1];
+	replaceFile.append(".replace");
+	outputFile.open(replaceFile);
+	if (!outputFile.is_open()) {
+		std::cout << "Failed to open the file.\n";
+		return (1);
+	}
+
+	while (true) {
+		std::getline(inputFile, line);
+		size_t pos = 0;
+		while (true) {
+			pos = line.find(s1, pos);
+			if (pos == std::string::npos) {
+				break ;
+			}
+			line.erase(pos, s1Len);
+			line.insert(pos, s2);
+			pos += s2Len;
+		}
+		outputFile << line;
+		if (inputFile.eof()) {
+			break ;
+		}
+		outputFile << std::endl;
+	}
+	inputFile.close();
+	outputFile.close();
+
+	return 0;
+}
---
+++ cppModule/cpp01/ex05/Harl.cpp
@@ -0,0 +1,28 @@
+#include "Harl.hpp"
+
+void Harl::debug( void ) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n";
+}
+
+void Harl::info( void ) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning( void ) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+}
+
+void Harl::error( void ) {
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain( std::string level ) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*funcPtr[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			(this->*funcPtr[i])();
+		}
+	}
+}
